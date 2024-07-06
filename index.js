b7fa9405cb558be64582a437b1bfd6e06ea7c46e
YashVar304 <yash21153028@akgec.ac.in>
2024-07-06 10:43:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("Yash Varshney")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
